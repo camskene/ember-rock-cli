3a3f83e89073c41f37ceef1e418a146060d2d87d
Cam Skene <camskene@gmail.com>
2016-01-18 18:06:21 -0500

    set dummy date as model property on the route
    - previously we had defined a property on the controller and assigned the dummy data to that propery
    -

--- app/routes/songs.js
+++ app/routes/songs.js
@@ -1,4 +1,47 @@
 import Ember from 'ember';
 
+// create Song 'model'
+var Song = Ember.Object.extend({
+  title: '',
+  rating: 0,
+  band: ''
+});
+
+// create instances of Song with create method
+var blackDog = Song.create({
+  title: 'Black Dog',
+  band: 'Led Zeppelin',
+  rating: 3
+});
+
+var yellowLedbetter = Song.create({
+  title: 'Yellow Ledbetter',
+  band: 'Pearl Jam',
+  rating: 4
+});
+
+var pretender = Song.create({
+  title: 'The Pretender',
+  band: 'Foo Fighters',
+  rating: 2
+});
+
+
+// Define a 'collection' to hold instances of Song
+var SongCollection = Ember.Object.extend({
+  content: [],
+  sortProperties: ['rating:desc'],
+  sortedContent: Ember.computed.sort('content', 'sortProperties')
+});
+
+// instantiate SongCollection
+var songs = SongCollection.create();
+
+// add our songs to the collection
+songs.get('content').pushObjects([blackDog, yellowLedbetter, pretender]);
+
 export default Ember.Route.extend({
+  model: function() {
+    return songs;
+  }
 });

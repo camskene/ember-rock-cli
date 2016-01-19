ca42f0549fb343bde509d2b6381466b4763a84e6
Cam Skene <camskene@gmail.com>
2016-01-19 01:34:57 -0500

    handle createBand action

--- app/routes/bands.js
+++ app/routes/bands.js
@@ -57,5 +57,21 @@ bands.get('content').pushObjects([ledZeppelin, pearlJam, fooFighters]);
 export default Ember.Route.extend({
   model: function() {
     return bands;
+  },
+
+  actions: {
+    createBand: function() {
+      // get name value that was set on the controller via the input
+      var name = this.get('controller').get('name');
+
+      // create new Band and set it's name attribute
+      var band = Band.create({name: name});
+
+      // add our band to the BandsCollection
+      bands.get('content').pushObject(band);
+
+      // reset name property to empty string
+      this.get('controller').set('name', '');
+    }
   }
 });

dec5892a2cfdef3fdb8db8a1f042081a6fefad5d
Cam Skene <camskene@gmail.com>
2016-01-20 12:50:13 -0500

    remove unused slug attribute

--- app/models/band.js
+++ app/models/band.js
@@ -4,9 +4,5 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr('string'),
   description: DS.attr('string'),
-  songs: DS.hasMany('song'),
-
-  slug: Ember.computed('name', function() {
-    return this.get('name').dasherize();
-  })
+  songs: DS.hasMany('song')
 });

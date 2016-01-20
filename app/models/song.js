89cb4d33fff2025e4585a5c00555bf018b8fa696
Cam Skene <camskene@gmail.com>
2016-01-20 12:23:32 -0500

    connect to JSON Api using Ember Data

--- app/models/song.js
+++ app/models/song.js
@@ -1,7 +1,8 @@
 import Ember from 'ember';
+import DS from 'ember-data';
 
 export default Ember.Object.extend({
-  title: '',
-  rating: 0,
-  band: null
+  title: DS.attr('string'),
+  rating: DS.attr('number'),
+  band: DS.belongsTo('band')
 });

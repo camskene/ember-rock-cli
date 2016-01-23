d950e358c312b422db50f76bed8fc5be206ef896
Cam Skene <camskene@gmail.com>
2016-01-23 13:57:03 -0500

    fix test

--- tests/acceptance/bands-test.js
+++ tests/acceptance/bands-test.js
@@ -17,6 +17,43 @@ test('List bands', function(assert) {
             attributes: {
               name: 'Radiohead'
             }
+          },
+          {
+            id: 2,
+            type: 'bands',
+            attributes: {
+              name: 'Long Distance Calling'
+            }
+          }
+        ]
+      };
+
+      return [200, {'Content-Type': 'applicaton/vnd.api+json'}, JSON.stringify(response)];
+    });
+  });
+
+
+  visit('/bands');
+
+  andThen(function() {
+    assert.equal(find('.band-link').length, 2, 'All band links are rendered');
+    assert.equal(find('.band-link:contains("Radiohead")').length, 1, 'First band link contains the band name');
+    assert.equal(find('.band-link:contains("Long Distance Calling")').length, 1, 'The other band link contains the band name');
+  });
+});
+
+
+test('Create a new band', function(assert) {
+  server = new Pretender(function() {
+    this.get('/bands', function() {
+      var response = {
+        data: [
+          {
+            id: 1,
+            type: 'bands',
+            attributes: {
+              name: 'Radiohead'
+            }
           }
         ]
       };
